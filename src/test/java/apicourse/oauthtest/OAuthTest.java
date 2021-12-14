c214892aecf664815767a7e175d6d1be68f05a67
Sonal Singh Solanki <sonalsolanki2707@gmail.com>
2021-12-14 22:50:51 +0530

    added code for oauth api end to end flow

--- src/test/java/apicourse/oauthtest/OAuthTest.java
+++ src/test/java/apicourse/oauthtest/OAuthTest.java
@@ -14,7 +14,7 @@ public class OAuthTest {
     @Test
     public void authTest() throws InterruptedException {
         //get auth code using selenium web browser
-        System.setProperty("webdriver.chrome.driver","src/test/driver/chromedriver");
+/*        System.setProperty("webdriver.chrome.driver","src/test/driver/chromedriver");
         WebDriver chromeDriver = new ChromeDriver();
         String email = "sonaltestcode28@gmail.com";
         String password = "testcode28";
@@ -25,20 +25,32 @@ public class OAuthTest {
         chromeDriver.findElement(By.cssSelector("input[type=password]")).sendKeys(password);
         chromeDriver.findElement(By.cssSelector("input[type=password]")).sendKeys(Keys.ENTER);
         Thread.sleep(4000);
+        //get current browser url
+        String url = chromeDriver.getCurrentUrl();*/
+        String url="https://rahulshettyacademy.com/getCourse.php?code=4%2F0AX4XfWhZl6skntKpLPzyYNvytA61ySB5WR8Li1C" +
+                "7KS2AeIk-Sh8AC_UB3GsgjJS54drO2A&scope=email+https%3A%2F%2F" +
+                "www.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=none#";
+        //split the url to get code,this will return an array,where we are storing second half after code ie 1st index
+        String partialCode=url.split("code=")[1];
+        //again split partial code till &scope at 0th index
+        String authCode = partialCode.split("&scope")[0];
+        System.out.println("authcode=="+authCode);
 
 
         //get access token
-       String accessTokenResponse = given().
-                queryParams("code","").
+       String accessTokenResponse = given().urlEncodingEnabled(false). //to disable any special character encoding
+                queryParams("code",authCode).
                 queryParams("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com").
                 queryParams("client_secret","erZOWM9g3UtwNRj340YYaK_W").
+               queryParams("grant_type", "authorization_code").
+                queryParams("state", "verifyfjdss").
+                queryParams("session_state", "ff4a89d1f7011eb34eef8cf02ce4353316d9744b..7eb8").
                 queryParams("redirect_uri", "https://rahulshettyacademy.com/getCourse.php").
-                queryParams("grant_type", "authorization_code").
                 when().log().all().
                 post("https://www.googleapis.com/oauth2/v4/token").
                 asString();
         JsonPath accessTokenJsonPath = ReusableMethods.rawToJson(accessTokenResponse);
-        String accessToken = accessTokenJsonPath.getString("acess_token");
+        String accessToken = accessTokenJsonPath.getString("access_token");
              //     queryParams("state", "verifyfjdss").
               //  queryParams("session_state", "ff4a89d1f7011eb34eef8cf02ce4353316d9744b..7eb8")
 
@@ -48,6 +60,6 @@ public class OAuthTest {
                 when().
                 get("https://rahulshettyacademy.com/getCourse.php").
                 asString();
-        System.out.println(response);
+        System.out.println("getcourse response= "+response);
     }
 }
